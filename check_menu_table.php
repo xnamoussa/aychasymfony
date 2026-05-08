cea0457d9f038e973a9c99dee02c4aabe41f4092
xnamoussa <emna.awini@esprit.tn>
2026-05-08 23:27:38 +0100

    update

--- check_menu_table.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=event_platform', 'root', '');
-$stmt = $pdo->query("SHOW CREATE TABLE menu");
-echo $stmt->fetch(PDO::FETCH_ASSOC)['Create Table'] . "\n";
--- scratch.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = 'templates/admin_evenement/show.html.twig';
-$lines = file($file);
-unset($lines[78]);
-file_put_contents($file, implode('', $lines));
-echo "Line 79 removed.";
