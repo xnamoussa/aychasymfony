98bdcc5c223cc8d9e28b7059855068e7f83e29c8
xnamoussa <emna.awini@esprit.tn>
2026-05-08 23:31:57 +0100

    up

--- find_fk_details.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=event_platform', 'root', '');
-$stmt = $pdo->prepare('SELECT TABLE_NAME, COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME 
-                       FROM information_schema.KEY_COLUMN_USAGE 
-                       WHERE CONSTRAINT_NAME = ? AND TABLE_SCHEMA = ?');
-$stmt->execute(['FK_3781EC10806F0F5C', 'event_platform']);
-print_r($stmt->fetch(PDO::FETCH_ASSOC));
